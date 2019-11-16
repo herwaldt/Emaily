b357ceef207cc9b7aa3904ce7e7e372cac5e80c6
herwaldt <1995ekaj@gmail.com>
2019-11-16 08:38:29 -0500

    tweak google strategy settings

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,4 @@
-//jake-prod EKEoZY3DeEKEoZY3DeMYb0hCUMYb0hCU
-//mongodb+srv://jake-prod:EKEoZY3DeEKEoZY3DeMYb0hCUMYb0hCU@cluster0-8w1e2.mongodb.net/test?retryWrites=true&w=majority
-
-//clientID 831009016007-u28gbbmr792gu20g6v1p5jfu5g9ccqtf.apps.googleusercontent.com
-//client secret ZRACgpKIT3O0Y_LzETDWfQDE
+//https://serene-brushlands-60511.herokuapp.com/
 
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
